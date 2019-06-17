c504dcfa03a80e478a2d11b792e8629b412ad528
Tomasz Waszczyk <tomasz@waszczyk.com>
2019-06-17 22:40:55 +0200

    Added gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
+    title: 'thirdwave.network',
+    description: 'thirdwave.network - the third wave of digitalization',
     siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
   },
   mapping: {
