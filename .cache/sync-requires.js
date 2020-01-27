c78e193c0f5eee6bcbe4f0905e22a1de6914e477
Tomasz Waszczyk <tomasz.waszczyk@relayr.io>
2020-01-27 13:29:21 +0100

    Added license

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,17 +5,17 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---src-templates-posts-author-template-tsx": hot(preferDefault(require("/home/panta/workspaces/thirdwave-network-website/src/templates/posts/author.template.tsx"))),
-  "component---src-templates-pages-articles-template-tsx": hot(preferDefault(require("/home/panta/workspaces/thirdwave-network-website/src/templates/pages/articles.template.tsx"))),
-  "component---src-templates-posts-article-template-tsx": hot(preferDefault(require("/home/panta/workspaces/thirdwave-network-website/src/templates/posts/article.template.tsx"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/panta/workspaces/thirdwave-network-website/.cache/dev-404-page.js"))),
-  "component---src-pages-404-tsx": hot(preferDefault(require("/home/panta/workspaces/thirdwave-network-website/src/pages/404.tsx"))),
-  "component---src-pages-about-tsx": hot(preferDefault(require("/home/panta/workspaces/thirdwave-network-website/src/pages/about.tsx"))),
-  "component---src-pages-careers-tsx": hot(preferDefault(require("/home/panta/workspaces/thirdwave-network-website/src/pages/careers.tsx"))),
-  "component---src-pages-contact-tsx": hot(preferDefault(require("/home/panta/workspaces/thirdwave-network-website/src/pages/contact.tsx"))),
-  "component---src-pages-index-tsx": hot(preferDefault(require("/home/panta/workspaces/thirdwave-network-website/src/pages/index.tsx"))),
-  "component---src-pages-labs-index-tsx": hot(preferDefault(require("/home/panta/workspaces/thirdwave-network-website/src/pages/labs/index.tsx"))),
-  "component---src-pages-labs-novela-tsx": hot(preferDefault(require("/home/panta/workspaces/thirdwave-network-website/src/pages/labs/novela.tsx"))),
-  "component---src-pages-preview-tsx": hot(preferDefault(require("/home/panta/workspaces/thirdwave-network-website/src/pages/preview.tsx")))
+  "component---src-templates-posts-author-template-tsx": hot(preferDefault(require("/home/twaszczyk/workspaces/thirdwave-network-website/src/templates/posts/author.template.tsx"))),
+  "component---src-templates-pages-articles-template-tsx": hot(preferDefault(require("/home/twaszczyk/workspaces/thirdwave-network-website/src/templates/pages/articles.template.tsx"))),
+  "component---src-templates-posts-article-template-tsx": hot(preferDefault(require("/home/twaszczyk/workspaces/thirdwave-network-website/src/templates/posts/article.template.tsx"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/twaszczyk/workspaces/thirdwave-network-website/.cache/dev-404-page.js"))),
+  "component---src-pages-404-tsx": hot(preferDefault(require("/home/twaszczyk/workspaces/thirdwave-network-website/src/pages/404.tsx"))),
+  "component---src-pages-about-tsx": hot(preferDefault(require("/home/twaszczyk/workspaces/thirdwave-network-website/src/pages/about.tsx"))),
+  "component---src-pages-careers-tsx": hot(preferDefault(require("/home/twaszczyk/workspaces/thirdwave-network-website/src/pages/careers.tsx"))),
+  "component---src-pages-contact-tsx": hot(preferDefault(require("/home/twaszczyk/workspaces/thirdwave-network-website/src/pages/contact.tsx"))),
+  "component---src-pages-index-tsx": hot(preferDefault(require("/home/twaszczyk/workspaces/thirdwave-network-website/src/pages/index.tsx"))),
+  "component---src-pages-labs-index-tsx": hot(preferDefault(require("/home/twaszczyk/workspaces/thirdwave-network-website/src/pages/labs/index.tsx"))),
+  "component---src-pages-labs-novela-tsx": hot(preferDefault(require("/home/twaszczyk/workspaces/thirdwave-network-website/src/pages/labs/novela.tsx"))),
+  "component---src-pages-preview-tsx": hot(preferDefault(require("/home/twaszczyk/workspaces/thirdwave-network-website/src/pages/preview.tsx")))
 }
 
