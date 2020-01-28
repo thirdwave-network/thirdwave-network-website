afe772147ff8adf98ee25e00e14468dc22bc8196
Tomasz Waszczyk <tomasz.waszczyk@relayr.io>
2020-01-28 12:33:38 +0100

    Init of server

--- src/server/api.js
+++ src/server/api.js
@@ -1,19 +1,45 @@
-var http = require('http');
+let express = require("express"),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    nodeMailer = require('node-mailer');
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(process.env.PORT || 5000); //the server object listens on port 8080
+let app = express();
 
-const baseUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://thirdwave-network-mailer.herokuapp.com/api'
-    : 'http://localhost:8080/api'
+let server = app.listen(process.env.PORT || 5000, function(){
+  let port = server.address().port;
+  console.info("Server started at http://localhost:%s", port);
+});
 
-function buildUrl(url) {
-  return baseUrl + url
-}
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.post('/api', function (req, res) {
+  let transporter = nodeMailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+          // should be replaced with real sender's account
+          user: 'hello@gmail.com',
+          pass: 'test'
+      }
+  });
+  let mailOptions = {
+      // should be replaced with real recipient's account
+      to: 'info@gmail.com',
+      subject: req.body.subject,
+      text: req.body.message
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+  res.writeHead(301, { Location: 'index.html' });
+  res.end();
+});
+
 
 function buildHeaders() {
   const headers = {}
