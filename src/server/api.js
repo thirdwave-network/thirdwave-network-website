7ebb620aaedc3ec50f1e09c328e26a00d7fbb347
Tomasz Waszczyk <tomasz.waszczyk@relayr.io>
2020-01-28 19:30:44 +0100

    Added FETCH

--- src/server/api.js
+++ src/server/api.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,FETCH');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
